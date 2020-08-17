38084f7849f8bc0829a835e39fbeef5edee6f839
Arnav Gosain <arn4v@tutanota.com>
2020-08-17 05:57:40 +0530

    syncify-server: querystring is a part of the nodejs api

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
+import * as authUiRouter from "./auth_ui/router";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "@feathersjs/express";
-import router from "./router";
+import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || 8888;
 const app = express();
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(path.resolve(__dirname), "../public")));
 app.use(cors());
 app.use(cookieParser());
-
-app.use(router);
+app.use(authUiRouter.default);
 
 app.listen(PORT, () => {
     console.log(`Listening at port ${PORT}`);
